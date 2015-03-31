c1d200a0f1b170a481eee674bcfb0b462c2e5c4c
ericsong <regonics@gmail.com>
2015-03-31 19:44:23 -0400

    updated schemas

--- TestSchema.js
+++ TestSchema.js
@@ -5,11 +5,26 @@ var TestSchema = mongoose.Schema({
 	questions: Array
 });
 
+/*
+ * ~~Question Schema~~
+ * number - Question's number (order in which it is shown)
+ * category - Type of question (ex. Personal, Info)
+ * priority - Question's importance
+ * body - question text
+ * choices - array of choice objets with text, correctness, and value
+ *  {
+ *    text: "sample choice", //choice text
+ *    correct: false, //true = correct, false = not correct (false when theres no correct answer)
+ *    value: 20 //point value
+ *  }
+ *  max_answers - maximum number of choices allowed to be selected
+ */
 var QuestionSchema = mongoose.Schema({
 	number: Number,
+  category: String,
+  priority: Number,
 	body: String,
 	choices: Array,
-	correct_answer: Array,
 	max_answers: Number	
 });
 
--- UserSchema.js
+++ UserSchema.js
@@ -1,10 +1,28 @@
 var mongoose = require('mongoose');
 
+/*
+ * ~~User Schema~~
+ * first_name - first name
+ * last_name - last name
+ * id - unique patient ID
+ * tests - all of user's recorded tests
+ * {
+ *  date: Number, //unix timestamp
+ *  score: Number //calculated overall score
+ *  answers: [answer, answer, answer]
+ * }
+ * answer: {
+ *   id: question id,
+ *   priorityScore: Number, //priority score for doctors to bring up with patients
+ *   answers: []
+ * }
+ *
+ */
 var UserSchema = mongoose.Schema({
 	first_name: String,
 	last_name: String,
 	id: {
-		type: Number,
+		type: String,
 		unique: true
 	},
 	tests: Array
